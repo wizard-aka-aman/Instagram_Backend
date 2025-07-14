8a5100ac63d3a6113c5844e3b1c9714fa3f5077c
bhavesh <k.bhavesh@sanedhealth.com>
2025-07-14 10:49:03 +0530

    Removed appsettings.json from version control

---
+++ Model/InstagramContext.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Instagram.Model
+{
+    public class InstagramContext :DbContext
+    {
+
+
+        public InstagramContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+         
+
+    }
+}
